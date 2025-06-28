21736ffba6c5111c1cef7cd53ce1c1d51797ab6d
Trim Bresilla <trim.bresilla@gmail.com>
2025-06-28 11:28:58 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,9 @@
+
+<img align="right" width="26%" src="./misc/logo.png">
+
+Zoneout
+===
+
+A C++ header-only library for managing robots operation zones and spaces based on raster and vector maps
+
+---
---
+++ examples/main.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+#include <thread>
+
+#include "concord/concord.hpp"
+#include "geoson/geoson.hpp"
+#include "geotiv/geotiv.hpp"
+#include "rerun/recording_stream.hpp"
+
+#include "zoneout/zoneout.hpp"
+
+int main() { return 0; }
